c4f157061b89d6d48f5abd8000e792951aa79683
Abhishek pal <144125359+pala72748@users.noreply.github.com>
2024-02-15 13:06:04 +0530

    Productcategory.js

--- models/Productcategory.js
+++ models/Productcategory.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/furniture', {
+mongoose.connect('mongodb+srv://pala72748:root@home.7mh2fle.mongodb.net/furniture?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
